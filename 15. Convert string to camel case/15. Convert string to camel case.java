52834d01955489becdf68aae1a1fec9a9b272658
Ionut Boghean <johnny.boghean.developer@gmail.com>
2021-07-26 15:27:22 +0200

    15 -> java

---
+++ 15. Convert string to camel case/15. Convert string to camel case.java
@@ -0,0 +1,18 @@
+// 15 -> Convert string to camel case
+// https://www.codewars.com/kata/517abf86da9663f1d2000003/train/
+
+import java.lang.StringBuilder;
+class Solution{
+  static String toCamelCase(String s){    
+    s = s.replaceAll("-", "_");
+    String [] a = s.split("_");
+    
+    s = "";
+    s += a[0];
+    
+    for (int i=1; i<a.length; i++)      
+      s += a[i].substring(0, 1).toUpperCase() + a[i].substring(1).toLowerCase();
+    
+    return s;
+  }
+}
