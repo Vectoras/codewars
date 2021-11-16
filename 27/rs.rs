7a3df8fded7a61c0a9e865a8166a55f23e942e06
Ionut Boghean <johnny.boghean.developer@gmail.com>
2021-11-16 19:42:02 +0000

    27 -> rust

---
+++ 27/rs.rs
@@ -0,0 +1,16 @@
+// 27. Vowel Count
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+fn get_count(string: &str) -> usize {
+  let mut vowels_count: usize = 0;
+  
+  let mut vowels_list = vec!["a", "e", "i", "o", "u"];
+  
+  for i in string.split(""){
+      if vowels_list.contains(&i) {
+          vowels_count += 1;
+      }
+  }
+  
+  vowels_count
+}
