72ba0d038c62afd5a8ff92770e29703ea1289e44
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-01-11 08:28:18 +0000

    38 - rust

---
+++ 38/rs.rs
@@ -0,0 +1,10 @@
+// 38 - Descending Order
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+fn descending_order(x: u64) -> u64 {
+  let mut digits: Vec<_> = x.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+  digits.sort();
+  digits.reverse();
+  let mut results: String = digits.iter().map( |&id| id.to_string()).collect(); 
+  return results.parse::<u64>().unwrap();
+}
