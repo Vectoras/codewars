6fa1f1cb8c1d46756d88f994d7adb4fa5cdbf8b0
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-01-19 12:58:02 +0000

    41 - rust

---
+++ 41/rs.rs
@@ -0,0 +1,11 @@
+// 41 - Grasshopper - Summation
+// https://www.codewars.com/kata
+
+fn summation(n: i32) -> i32 {    
+  let mut sum:i32 = 0;    
+  for i in 1..n+1 {
+      println!("i: {}", i);
+      sum += i;
+  }    
+  return sum;
+}
