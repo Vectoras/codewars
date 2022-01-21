7da4d2998f7077027010a76c50944fe61fbfb71e
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-01-21 15:15:34 +0000

    52 - rust

---
+++ 52. Exes and Ohs/rs.rs
@@ -0,0 +1,14 @@
+// 52. Exes and Ohs
+// https://www.codewars.com/kata/55908aad6620c066bc00002a
+
+fn xo(string: &'static str) -> bool {    
+  let mut x :i32 = 0;
+  let mut o :i32 = 0;
+  
+  for c in string.chars() {
+      if &c.to_lowercase().to_string() == "x" { x += 1; }
+      else if &c.to_lowercase().to_string() == "o" { o += 1; }
+  }
+  
+  return x == o;
+}
